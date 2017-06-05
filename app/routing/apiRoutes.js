0c7777557957e332bf16c0fe364636303ddec5f1
L. Rochelle Beal <beal.rochelle@gmail.com>
2017-06-04 20:35:01 -0500

    Update apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,47 +3,40 @@
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on friends-data.
 // ===============================================================================
-
-var frindsData = require("../data/friend");
+var friendData = require("../data/friend");
+//console.log(friendData);
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 module.exports = function(app) {
+  // ===============================================================================
   // API GET Requests
   // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // When a user visits a link
+  // (ex: localhost:PORT/api/friends... they are shown a JSON of the data)
   // ---------------------------------------------------------------------------
-
+  
   app.get("/api/friends", function(req, res) {
-    res.json(friendsData);
+    res.json(friendData);
   });
 
-
+  // ===============================================================================
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
   // (ex. User fills out a friend request... this data is then sent to the server...
-  // Then the server saves the data to the friendsData array)
+  // Then the server saves the data to the friendData array)
   // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
     // Note the code here. Our "server" will respond to the survey.
-    //if (friendData.length < 10) {
-      friendsData.push(req.body);
+      friendData.push(req.body);
       res.json(true);
-    //}
   });
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function() {
-    // Empty out the arrays of data
-    friendData = [];
-    console.log(friendData);
-  });
 };
+
+
+                
