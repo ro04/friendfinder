8a49c4e22e334213af09b9323b2c7d6267e3e23b
L. Rochelle Beal <beal.rochelle@gmail.com>
2017-06-04 20:38:52 -0500

    Update friend.js

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,36 +1,57 @@
 // ===============================================================================
 // DATA
+// Below data will hold all of the friends data.
+// Initially we just set it equal to a "dummy" data.
+// But you could have it be an empty array as well.
 // ===============================================================================
 
 var friendArray = [
     {   
-        "name": "",
-        "photo": "",
-        "scores":["5","5", "5", "5", "5", "5", "5", "5", "5", "5"]
+        "name": "Cameron Jibril Thomaz",
+        "photo": "http://thesource.com/wp-content/uploads/2016/02/wiz-khalifa-15.jpg",
+        "scores": ["5","1", "4", "5", "4", "1", "2", "5", "3", "5"]
     },
 
     {   
-        "name": "",
-        "photo": "",
-        "scores":["4","4", "4", "4", "4", "4", "4", "4", "4", "4"]
+        "name": "Destiny Hope Cyrus",
+        "photo": "http://us.hellomagazine.com/imagenes/profiles/miley-cyrus/6035-miley-cyrus.jpg",
+        "scores": ["1","4", "4", "4", "4", "4", "4", "4", "4", "4"]
     },
 
     {   
-        "name": "",
-        "photo": "",
-        "scores":["3","3", "3", "3", "3", "3", "3", "3", "3", "3"]
+        "name": "Onika Tany Maraj",
+        "photo": "http://img.usmagazine.com/social/nicki-minaj-24797003-9aa4-4a38-a00c-f0aa5d6fa177.jpg",
+        "scores": ["5","5", "5", "5", "1", "1", "3", "1", "1", "1"]
     },
 
     {   
-        "name": "",
-        "photo": "",
-        "scores":["2","2", "2", "2", "2", "2", "2", "2", "2", "2"]
+        "name": "Peter Gene Bayot Hernandez",
+        "photo": "https://cbsmix965houston.files.wordpress.com/2011/09/113194941.jpg",
+        "scores": ["4","1", "4", "3", "3", "3", "4", "2", "3", "2"]
     },
 
     {   
-        "name": "",
-        "photo": "",
-        "scores":["1","1", "1", "1", "1", "1", "1", "1", "1", "1"]
+        "name": "Amethyst Amelia Kelly",
+        "photo": "http://l450v.alamy.com/450v/e2r0ra/iggy-azalea-real-name-amethyst-amelia-kelly-leaves-the-shepherds-bush-e2r0ra.jpg",
+        "scores": ["4","5", "3", "3", "2", "5", "5", "5", "5", "2"]
+    },
+
+    {
+        "name": "Stefani Joanne Angel Germanotta",
+        "photo": "http://images6.fanpop.com/image/quiz/968000/968769_1358559463703_226_300.jpg",
+        "scores": ["1","2", "1", "2", "1", "2", "1", "2", "1", "2"]
+    },
+
+    {
+        "name": "Robert Allen Zimmerman",
+        "photo": "http://rock95.com/wp-content/uploads/2017/03/BOB-dylan-2-1000x500-600x300.jpg",
+        "scores": ["2","3", "1", "1", "3", "2", "3", "1", "3", "4"]
+    },
+
+    {
+        "name": "Marshell Mathers III",
+        "photo": "http://3.bp.blogspot.com/-tr_gHuvRYDU/TpxFXsVesiI/AAAAAAAAAoI/auvYdsbhW1w/s1600/Marshall%2BMathers.jpg",
+        "scores": ["4","2", "5", "2", "4", "3", "5", "2", "3", "2"]
     }
 
 ];
